02c4e559ef79ad6b0f9c04ade8bc7acb42d99ed4
Lakshay Gambhir <lakshaygambhir96@gmail.com>
2025-12-28 17:52:35 +0000

    datatype learning

--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,18 @@
+"use strict"; //new standard of js being used
 console.log("Hello World")
+//alert("Hello")
+let name = "Lakshay"
+let age = 19
+let isloggedin = false
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol
+
+// object 
+
+console.log(typeof(name))
+console.table([name,age,isloggedin])
